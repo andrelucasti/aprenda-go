2376459c0a0d80e012b14648d28718c6c53af7d9
andre.silva <andre.silva@oifood.com.br>
2021-11-11 13:55:38 -0300

    Ex1 prints

---
+++ ex1/ex1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("Hi, my name is ", y, "and I've", x, "years old", "and it's", z, ".")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
