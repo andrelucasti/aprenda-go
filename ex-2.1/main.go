e2deb41c1fcb901879b6f2bb958c84284073c5b0
Andre Lucas Santos Silva <andre.silva@ifood.com.br>
2021-11-30 02:58:37 -0300

    Exercícios: Nível #2 - 1

---
+++ ex-2.1/main.go
@@ -0,0 +1,15 @@
+package main
+
+//https://www.youtube.com/watch?v=onK_nd--g1g&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=31
+
+//Cap. 5 – Exercícios: Nível #2 – 1
+import "fmt"
+
+const n = 255
+
+func main() {
+
+	fmt.Printf("%b\t\n", n)
+	fmt.Printf("%#x\t\n", n)
+	fmt.Printf("%d\t\n", n)
+}
